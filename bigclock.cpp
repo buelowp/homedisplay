da1310c6a586c424a1c3d01574356be530ac2e8e
Peter Buelow <goballstate@gmail.com>
2022-11-17 14:08:27 -0600

    Fixing missing commit

---
+++ bigclock.cpp
@@ -0,0 +1,63 @@
+// SPDX-FileCopyrightText: 2022 <copyright holder> <email>
+// SPDX-License-Identifier: MIT
+
+#include "bigclock.h"
+
+BigClock::BigClock(QFrame *parent) : QFrame(parent)
+{
+    m_layout = new QGridLayout();
+    m_primaryClock = new QLabel();
+    m_primaryClock->setScaledContents(true);
+    m_primaryClock->setAlignment(Qt::AlignCenter);
+    m_primaryDate = new QLabel();
+    m_primaryDate->setAlignment(Qt::AlignCenter);
+    m_primaryDate->setScaledContents(true);
+
+    QFont c("Roboto-Regular", 120);
+    QFont d("Roboto-Regular", 32);
+
+    m_primaryClock->setFont(c);
+    m_primaryDate->setFont(d);
+
+    m_layout->addWidget(m_primaryClock, 0, 0, 3, 1);
+    m_layout->addWidget(m_primaryDate, 3, 0, 1, 1);
+
+    setLayout(m_layout);
+
+    m_updateTimer = new QTimer(this);
+    connect(m_updateTimer, &QTimer::timeout, this, &BigClock::updateClock);
+    m_updateTimer->setInterval(1000);
+}
+
+BigClock::~BigClock()
+{
+}
+
+void BigClock::showEvent(QShowEvent* e)
+{
+    qDebug() << __PRETTY_FUNCTION__;
+    updateClock();
+}
+
+void BigClock::begin()
+{
+    m_updateTimer->start();
+}
+
+void BigClock::end()
+{
+    m_updateTimer->stop();
+}
+
+void BigClock::updateClock()
+{
+    QDateTime now = QDateTime::currentDateTime();
+
+    if (now.date() != m_time.date()) {
+        m_primaryDate->setText(now.date().toString("dddd MMMM d, yyyy"));
+    }
+    if (now.time() != m_time.time()) {
+        m_primaryClock->setText(now.time().toString("h:mm A"));
+    }
+    m_time = now;
+}
---
+++ bigclock.h
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: 2022 <copyright holder> <email>
+// SPDX-License-Identifier: MIT
+
+#ifndef BIGCLOCK_H
+#define BIGCLOCK_H
+
+#include <QtCore/QtCore>
+#include <QtWidgets/QtWidgets>
+
+/**
+ * @todo write docs
+ */
+class BigClock : public QFrame
+{
+    Q_OBJECT
+
+public:
+    BigClock(QFrame *parent = nullptr);
+    ~BigClock();
+
+    void begin();
+    void end();
+
+public slots:
+    void updateClock();
+
+protected:
+    void showEvent(QShowEvent *e) override;
+
+private:
+    QGridLayout *m_layout;
+
+	QLabel *m_primaryClock;
+	QLabel *m_primaryDate;
+    QTimer *m_updateTimer;
+    QDateTime m_time;
+};
+
+#endif // BIGCLOCK_H
